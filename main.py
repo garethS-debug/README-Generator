8cf8d19247e458354ab2d5284ee8b1c0a91b8854
garethS-debug <76433148+garethS-debug@users.noreply.github.com>
2026-02-19 23:11:10 +0000

    Setting up the modules

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ from InquirerPy import prompt
 from rich.console import Console
 from rich.table import Table
 from rich.progress import Progress
+from qtoask import question
 
-console = Console()
+# console = Console()
+answers = prompt(question())
+content = f"""
+            #{answers['title']}
+            # Description
+            # {answers['description']}
+            """
 
-# get user inputs
-questions = [
-    {"type": "input", "name": "title", "message": "What is your project title?"},
-    {"type": "input", "name": "description", "message": "What is the description?"},
-]
 
-answers = prompt(questions)
 
-# Display a formatted message with Rich
-console.print(
-    f"Hello, [bold {answers['title']}] {answers['description']}![/bold {answers['description']}]"
-)
+# # Display a formatted message with Rich
+# console.print(
+#     f"Hello, [bold {answers['title']}] {answers['description']}![/bold {answers['description']}]"
+# )
--- qtoask.py
+++ qtoask.py
@@ -1,1 +1,6 @@
-from InquirerPy import prompt
+def question():
+    # get user inputs
+    return [
+        {"type": "input", "name": "title", "message": "What is your project title?"},
+        {"type": "input", "name": "description", "message": "What is the description?"},
+    ]
