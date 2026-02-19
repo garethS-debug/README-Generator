b3d07dca99a8fe21e8f1cbb556940597a3533ae1
garethS-debug <76433148+garethS-debug@users.noreply.github.com>
2026-02-19 22:51:50 +0000

    Importing project requirements

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from InquirerPy import prompt
+from rich.console import Console
+from rich.table import Table
+from rich.progress import Progress
+
+# get user inputs
+questions = [
+    {"type": "input", "name": "Project Title", "message": "What is your project title?"},
+    {"type": "input", "name": "color", "message": "What is your favorite color?"},
+]
